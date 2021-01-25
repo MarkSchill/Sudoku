024834a8c366fd31bf9aab403e257768e761f702
Mark Schill <markschill519@gmail.com>
2021-01-24 21:31:17 -0600

    Added the weird graphic inside of the 'circles' window

--- menu.c
+++ menu.c
@@ -1,6 +1,9 @@
 #include "menu.h"
 
 #include <ncurses.h>
+#include <stdlib.h>
+#include <memory.h>
+#include <time.h>
 #include "keyboard.h"
 #include "game.h"
 
@@ -47,36 +50,54 @@ void menu_input(int *menu_running, int *y, int *x, int *my, int *mx) {
 
 void menu_draw(int *y, int *x, int *my, int *mx) {
 	int width = 30;
-	int height = 11;
+	int height = 13;
 	int box_x = (*mx / 2) - (width / 2);
 	int box_y = (*my / 2) - (height / 2);
 
 	WINDOW *middle = newwin(height, width, box_y, box_x);
 	box(middle, 0, 0);
 
-	WINDOW *circles = newwin(9, 9, box_y + 1, box_x + 18);
+	WINDOW *circles = newwin(11, 11, box_y + 1, box_x + 16);
 	box(circles, 0, 0);
 
-	// static int data[9];
+	wattron(middle, A_BOLD | A_UNDERLINE);
+	mvwprintw(middle, 1, 1, "Sudoku");
+	wattroff(middle, A_BOLD | A_UNDERLINE);
 
 	int i;
 	for (i = 0; i < n_items; i++) {
-		mvwprintw(middle, i + 1, 1, "[%c] %s", (i == *y ? 'x' : ' '), items[i]);
+		mvwprintw(middle, i + 2, 1, "[%c] %s", (i == *y ? 'x' : ' '), items[i]);
 	}
 
+	static int data[81];
+
+	data[rand() % 82] = 1;
+
+	int filled = 1;
+	for (i = 0; i < sizeof(data)/sizeof(int); i++) {
+		if (data[i] == 0) {
+			filled = 0;
+			break;
+		}
+	}
+
+	if (filled) {
+		bzero(data, sizeof(data));
+	}
+
+	// Printing the random tiles in the circles window
+	int cy, cx;
+	for (cy = 0; cy < 9; cy++) {
+		for (cx = 0; cx < 9; cx++) {
+			if (data[cy * 9 + cx]) {
+				mvwprintw(circles, cy + 1, cx + 1, "X");
+			}
+		}
+	}
+
+	refresh();
 	wrefresh(middle);
-
-
-	// Create timer to make sure this runs at a certain speed
-	// Highlight random tiles in the background of the 'circles' window
-
-	//for (i = 0; i < 9; i++) {
-		mvprintw(box_y + 1, box_x + 18, "X");
-	//}
-	
 	wrefresh(circles);
-
-	move(*y + box_y + 1, *x + box_x + 1);
 }
 
 void menu_loop(int *my, int *mx) {
@@ -92,6 +113,8 @@ void menu_loop(int *my, int *mx) {
 	// Create timeout on the keyboard input so that it allows the 'circles' window to repaint and update
 	// TODO: MAKE SURE TO TURN OFF THE TIMEOUT WHEN THE ACTUAL GAME STARTS
 	timeout(1000);
+
+	srand(time(NULL));
 
 	while (menu_running) {
 		menu_draw(&y, &x, my, mx);
